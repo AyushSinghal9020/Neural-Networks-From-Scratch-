d06eb0e4cb01d71370d94b9f6d905acf6241190e
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-03-29 05:56:55 +0530

    Create Python Source Code.py

---
+++ Optimizers /Losses /Categorical Cross Entropy/Python Source Code.py
@@ -0,0 +1,10 @@
+def CCE(predicted , actual):
+    
+    for _ in classes :
+        
+        for _ in list_of_values:
+        
+            error = 0
+            error += predicted * np.log1p(predicted)
+
+    return error
