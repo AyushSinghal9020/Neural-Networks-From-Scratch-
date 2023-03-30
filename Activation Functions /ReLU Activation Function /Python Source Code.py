713dfe380c1f200f1cd9cbc3720b655c697400a0
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-03-30 17:28:50 +0530

    Delete Activation Functions  directory

--- Activation Functions /ReLU Activation Function /Python Source Code.py
+++
@@ -1,7 +0,0 @@
-def relu(array):
-    output_array = np.empty(shape = array.shape)
-    for i in array:
-        output_array = np.hstack([output_array , max(0 , i)])
-        output_array = np.delete(output_array , 0 , 0)
-
-    return output_array
--- Activation Functions /Tanh Acitvation Function/Python Source Code.py
+++
@@ -1,8 +0,0 @@
-def tanh(array):
-    output_array = np.empty(shape = array.shape)
-    
-    for i in array:
-        output_array = np.hstack([new_array , ((m.e ** i) - (m.e ** (-i))) / ((m.e ** i) + (m.e ** (-i)))])
-        output_array = np.delete(output_array , 0 , 0)
-
-    return output_array
