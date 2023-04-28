8c805b1c91fcca8e85ed0588fe5e5f175ce01143
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-28 16:13:08 +0530

    Update Python Source Code.py

--- Optimizers /Gradient Descent/Python Source Code.py
+++ Optimizers /Gradient Descent/Python Source Code.py
@@ -1,13 +1,24 @@
 # METHOD 1
 
-weights = abs(np.random.randn(1))
-biases = abs(np.random.randn(1))
-losses = []
-for _ in range(100):
-    weights -= -2 * loss * 0.01
-    biases -= -2 * loss * 0.01
-    loss = (60 - (weights * 30 + biases))
-    losses.append(loss)
+def SGD(X , y):
+    
+    weights = abs(np.random.randn(X.shape[1]))
+    biases = abs(np.random.randn(1))
+
+    predic = []
+    losses = []
+
+    for _ in range(100):
+
+        pred = np.sum((weights * X).T) + biases
+        
+        loss = np.sum((pred - y) ** 2)
+        losses.append(loss)
+        
+        weights -= -2 * loss * 0.01
+        biases -= -2 * loss * 0.01
+
+    return weights , biases , losses
 
 # METHOD 2
 
