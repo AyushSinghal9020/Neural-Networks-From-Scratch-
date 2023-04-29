ff933a35a2e6872869f3748d42937cb4981a80a6
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-29 16:55:40 +0530

    Update Python Source Code.py

--- Optimizers/Adaptive Gradient Descent/Python Source Code.py
+++ Optimizers/Adaptive Gradient Descent/Python Source Code.py
@@ -1,36 +1,80 @@
-def adagrad(X , y , learning_rate = 0.01 , epsilon = 1e-7 , clip_norm = None , clip_value = None , ema = False):
+class adagrad:
+    def __init__(X , y , 
+                 learning_rate = 0.01 , epsilon = 1e-7 , 
+                 clip_norm = None , clip_value = None , 
+                 ema = False , ema_momentum = 0):
     
-    weights = abs(np.random.randn(X.shape[1]))
-    biases = abs(np.random.randn(1))
-
-    losses = []
-    grad = np.zeros(2)
-
-    for _ in range(100):
-
-        pred = np.sum((weights * X).T) + biases
-        
-        loss = np.sum((pred - y) ** 2)
-        losses.append(loss)
-        
-        grad[1] = -2 * loss
-
-        if ema: 
-
-            grad = ema_momentum * grad + (1 - ema_momentum) * grad
-
-        if clip_norm != None:
+        self.X = X
+        self.y = y 
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        self.clip_norm = clip_norm
+        self.clip_value = clip_value
+        self.ema = ema
+        self.ema_momentum = ema_momentum
+    
+    def build(self , weights = None , biases = None):
+    
+        if weights == None:
             
-            weights = np.clip(weights , weights , clip_norm)
-            biases = np.clip(biases , biases , clip_norm)
-
-        if clip_value != None:
-
-            grad = np.clip(grad , grad , clip_value)
+            self.weights = abs(np.random.randn(self.X.shape[1]))
         
-        weights -= np.sqrt(-1 / (grad[1] + epsilon)) * grad[0] * learning_rate 
-        biases -= np.sqrt(-1 / (grad[1] + epsilon)) * grad[0] * learning_rate
+        else :
+            
+            if weights.shape[0] == self.X.shape[1]:
+                
+                self.weights = weights
+            
+            else :
+                
+                raise UserWarning("Shapes do not match , initalizing random weights")
+                    
+                self.weights = abs(np.random.randn(self.X.shape[1]))
+        
+        if biases == None:
+            
+            self.biases = abs(np.random.randn(1))
+        
+        else :
+            
+            if biases.shape[0] == 1:
+                
+                self.biases = biases
+            
+            else :
+                
+                raise UserWarning("Shapes do not match , initalizing random biases")
+                    
+                self.biases = abs(np.random.randn(1))
 
-        grad[1] = grad[0]
+        self.losses = []
+        self.grad = np.zeros(2)
 
-    return weights , biases , losses
+    # for _ in range(100):
+
+    #     pred = np.sum((weights * X).T) + biases
+        
+    #     loss = np.sum((pred - y) ** 2)
+    #     losses.append(loss)
+        
+    #     grad[1] = -2 * loss
+
+    #     if ema: 
+
+    #         grad = ema_momentum * grad + (1 - ema_momentum) * grad
+
+    #     if clip_norm != None:
+            
+    #         weights = np.clip(weights , weights , clip_norm)
+    #         biases = np.clip(biases , biases , clip_norm)
+
+    #     if clip_value != None:
+
+    #         grad = np.clip(grad , grad , clip_value)
+        
+    #     weights -= np.sqrt(-1 / (grad[1] + epsilon)) * grad[0] * learning_rate 
+    #     biases -= np.sqrt(-1 / (grad[1] + epsilon)) * grad[0] * learning_rate
+
+    #     grad[1] = grad[0]
+
+    # return weights , biases , losses
